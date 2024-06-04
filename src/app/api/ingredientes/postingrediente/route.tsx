0a17cc37bbadc80afd87b8c2ad30eb229bffde18
Arvedson <tomasarvedson@gmail.com>
2024-06-04 11:18:41 -0600

    fix.16

--- src/app/api/ingredientes/postingrediente/route.tsx
+++ src/app/api/ingredientes/postingrediente/route.tsx
@@ -2,10 +2,16 @@ import { NextRequest, NextResponse } from 'next/server';
 import connectToDatabase from '../../../../lib/db/mongodb';
 import Ingrediente from '../../../../lib/db/models/Ingrediente';
 
+// Asegúrate de importar o definir 'uri' correctamente
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  throw new Error('El URI de MongoDB no está definido. Asegúrate de que la variable de entorno MONGODB_URI esté configurada.');
+}
 
 export async function POST(req: NextRequest) {
   try {
-    await connectToDatabase(uri: string); Promise<void>
+    await connectToDatabase(uri);
 
     const { nombre, categoria, sabor, textura, solidos, agua, costo, compatibilidad, picor } = await req.json();
     
