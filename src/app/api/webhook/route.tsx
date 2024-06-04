e6199284a33090c22cd0be64eb39dcd7e5a9a58a
Arvedson <tomasarvedson@gmail.com>
2024-06-04 17:48:31 -0600

    webhook data sync.4

--- src/app/api/webhook/route.tsx
+++ src/app/api/webhook/route.tsx
@@ -1,68 +1,62 @@
-import { Webhook } from 'svix';
-import { headers } from 'next/headers';
-import { NextRequest, NextResponse } from 'next/server';
+
 import mongoose from 'mongoose';
 import { createUser } from "@/lib/services/user.service";
 
-const connectMongo = async () => {
-  if (mongoose.connection.readyState === 0) {
-    await mongoose.connect(process.env.MONGODB_URI!);
-  }
-};
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
+import { WebhookEvent } from '@clerk/nextjs/server'
 
-export async function POST(req: NextRequest): Promise<NextResponse> {
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+export async function POST(req: Request) {
+
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
 
   if (!WEBHOOK_SECRET) {
-    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local');
+    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
 
+  // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
 
+  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new NextResponse('Error occurred -- no svix headers', {
-      status: 400,
-    });
+    return new Response('Error occured -- no svix headers', {
+      status: 400
+    })
   }
 
-  const payload = await req.json();
-  const { data } = payload;
-  const { first_name, last_name, email_addresses, primary_email_address_id } = data;
+  // Get the body
+  const payload = await req.json()
+  const body = JSON.stringify(payload);
 
-  // Ensure the email field is not empty
-  if (!email_addresses || !primary_email_address_id) {
-    console.error('Email addresses or primary email address ID is missing');
-    return new NextResponse('Invalid data received', {
-      status: 400,
-    });
-  }
+  // Create a new Svix instance with your secret.
+  const wh = new Webhook(WEBHOOK_SECRET);
 
-  const emailObject = email_addresses.find((e: any) => e.id === primary_email_address_id);
-  const email = emailObject?.email;
+  let evt: WebhookEvent
 
-  // Check if email is valid
-  if (!email) {
-    console.error('Primary email address not found');
-    return new NextResponse('Invalid email data received', {
-      status: 400,
-    });
-  }
-
+  // Verify the payload with the headers
   try {
-    await connectMongo();
-
-    const user = { name: `${first_name} ${last_name}`, email };
-    await createUser(user);
-    console.log(`User created successfully`);
-  } catch (error) {
-    console.error('Error saving user:', error);
-    return new NextResponse('Error occurred while saving user', {
-      status: 500,
-    });
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent
+  } catch (err) {
+    console.error('Error verifying webhook:', err);
+    return new Response('Error occured', {
+      status: 400
+    })
   }
 
-  return new NextResponse('', { status: 200 });
+  // Do something with the payload
+  // For this guide, you simply log the payload to the console
+  const { id } = evt.data;
+  const eventType = evt.type;
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
+  console.log('Webhook body:', body)
+
+  return new Response('', { status: 200 })
 }
