59a837b6eb5f57d038045033303250dfdfd120db
Arvedson <tomasarvedson@gmail.com>
2024-06-04 17:37:46 -0600

    webhook data sync.2

--- src/app/api/webhook/route.tsx
+++ src/app/api/webhook/route.tsx
@@ -17,7 +17,7 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local');
   }
 
-  const headerPayload = headers();
+  const headerPayload = headers(req);
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
@@ -30,7 +30,26 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
 
   const payload = await req.json();
   const { data } = payload;
-  const { first_name, last_name, email } = data;
+  const { first_name, last_name, email_addresses, primary_email_address_id } = data;
+
+  // Ensure the email field is not empty
+  if (!email_addresses || !primary_email_address_id) {
+    console.error('Email addresses or primary email address ID is missing');
+    return new NextResponse('Invalid data received', {
+      status: 400,
+    });
+  }
+
+  const emailObject = email_addresses.find((e: any) => e.id === primary_email_address_id);
+  const email = emailObject?.email;
+
+  // Check if email is valid
+  if (!email) {
+    console.error('Primary email address not found');
+    return new NextResponse('Invalid email data received', {
+      status: 400,
+    });
+  }
 
   try {
     await connectMongo();
