9f53c70895d1f57e7ad5cf68428cf248c0fbcbd8
Arvedson <tomasarvedson@gmail.com>
2024-06-04 11:26:12 -0600

    fix.18

--- src/app/api/receta/postrecetas/route.tsx
+++ src/app/api/receta/postrecetas/route.tsx
@@ -1,11 +1,17 @@
-// /app/api/receta/postrecetas/route.ts
 import { NextResponse } from 'next/server';
 import connectToDatabase from '../../../../lib/db/mongodb';
 import Receta from '../../../../lib/db/models/Recetas';
 
+// Asegúrate de importar o definir 'uri' correctamente
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  throw new Error('El URI de MongoDB no está definido. Asegúrate de que la variable de entorno MONGODB_URI esté configurada.');
+}
+
 export async function POST(request: Request) {
   try {
-    await connectToDatabase();
+    await connectToDatabase(uri!);
     const body = await request.json();
     const newReceta = new Receta(body);
     await newReceta.save();
