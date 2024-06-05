7a476c5ffa39a759648b8dd743149e63d5a9d17f
Arvedson <tomasarvedson@gmail.com>
2024-06-05 16:02:42 -0600

    espectro.3

--- src/app/api/receta/updateEspectro/route.tsx
+++ src/app/api/receta/updateEspectro/route.tsx
@@ -1,47 +1,18 @@
+// pages/api/receta/updateespectro.ts
+
 import { NextApiRequest, NextApiResponse } from 'next';
-import connectToDatabase from '../../../../lib/db/mongodb';
-import Ingrediente from '../../../../lib/db/models/Ingrediente';
+import actualizarEspectro from '../../../../lib/services/actualizarEspectro'; // Asegúrate de que la ruta sea correcta según tu estructura de archivos
 
-
-
-export default async function actualizarEspectro(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Puedes manejar diferentes métodos HTTP aquí si es necesario
   if (req.method === 'POST') {
     try {
-      await connectToDatabase();
-      
-      const { ingredientes, tipo } = req.body;
-
-      if (!ingredientes || ingredientes.length === 0) {
-        return res.status(400).json({ error: 'Debe ingresar al menos un ingrediente.' });
-      }
-
-      const pesoTotal = ingredientes.reduce((sum: number, ing: { cantidad: number }) => sum + ing.cantidad, 0);
-
-      for (const ing of ingredientes) {
-        const ingrediente = await Ingrediente.findOne({ nombre: ing.ingrediente });
-
-        if (!ingrediente) {
-          return res.status(400).json({ error: `El ingrediente ${ing.ingrediente} no existe en la base de datos.` });
-        }
-
-        const proporcion = (ing.cantidad / pesoTotal) * 100;
-
-        if (!ingrediente.espectro[tipo]) {
-          ingrediente.espectro = { ...ingrediente.espectro, [tipo]: 0 };
-        }
-
-        ingrediente.espectro = { ...ingrediente.espectro, [tipo]: ingrediente.espectro[tipo] + proporcion };
-        await ingrediente.save();
-      }
-
-      res.status(200).json({ message: 'Espectros actualizados correctamente.' });
-
+      await actualizarEspectro(req, res); // Llama a la función actualizarEspectro con los mismos argumentos
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'Error actualizando los espectros de los ingredientes.' });
+      res.status(500).json({ error: 'Error en el servidor.' });
     }
   } else {
     res.status(405).json({ error: 'Método no permitido.' });
   }
-};
-
+}
